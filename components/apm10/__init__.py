11b3cfb0e4d442a03321274c4b0d7a08b9ed7ec4
synodriver <diguohuangjiajinweijun@gmail.com>
2025-08-03 21:49:34 +0800

    apm2000 compatible

--- components/apm10/__init__.py
+++ components/apm10/__init__.py
@@ -1,2 +1,4 @@
 # https://www.aosong.com/userfiles/files/media/APM10激光颗粒物传感器中文版%20A3-202407.pdf
+# https://www.aosong.com/userfiles/files/media/APM2000激光颗粒物传感器（中文版）%20A0-0822.pdf
+# works with APM10 and APM2000
 # pull down pin3 to use i2c
